64358e68a36b4c2d48b6723940db416f0ac0e7f9
Gde-G <gdguernica@gmail.com>
2022-09-10 20:27:38 -0300

    Update settings production

--- campo/settings/prod.py
+++ campo/settings/prod.py
@@ -10,3 +10,35 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'pasoancho.herokuapp.com'
 ]
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+LOGGIN = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters':{
+        'verbose':{
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineon)s] %(messages)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple':{
+            'format':"%(levelname)s %(messages)s"
+        },
+    },
+    'handlers':{
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers':{
+        'MYAPP':{
+            'handlers': ["console"],
+            'level': 'DEBUG'
+        },
+    }
+}
+
+#Heroku Settings
+django_on_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
