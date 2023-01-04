2cb6ea6357d4ffe927c1b9e1dc77e613af1a0cd6
GdG <96954645+Gde-G@users.noreply.github.com>
2023-01-04 20:26:54 -0300

    Update base.py

--- campo/settings/base.py
+++ campo/settings/base.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': ':memory:',
     }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
