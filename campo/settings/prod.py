0f6bd235ae057d8657bfafb344a0831a70b67ba1
Gde-G <gdguernica@gmail.com>
2023-01-04 21:19:56 -0300

    .

--- campo/settings/prod.py
+++ campo/settings/prod.py
@@ -7,7 +7,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = ['0.0.0.0:'+env.str('PORT'),
     'web-production-5ee9.up.railway.app',
     'pasoanchova.com'
 ]
