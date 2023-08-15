c152934edb7f138aac2495feff2cea2ef3f82c78
Gde-G <gdguernica@gmail.com>
2023-08-15 20:34:57 -0300

    remove some aws config

--- campo/settings/base.py
+++ campo/settings/base.py
@@ -36,8 +36,6 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
     'django_extensions',
     'captcha',
-    'whitenoise.runserver_nostatic'
-
 ]
 
 MIDDLEWARE = [
@@ -48,7 +46,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'campo.urls'
